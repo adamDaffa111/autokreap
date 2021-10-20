77ee1a15b22bf483ec7ec6bcf152a743dc1d3d46
adamdaffa111 <adamdaffa59@gmail.com>
2021-10-20 10:28:17 +0700

    fixing bug

--- script.js
+++ script.js
@@ -3,20 +3,41 @@ const countdownEl = document.querySelector("#countdown-container h1");
 const moveSound = document.querySelector('audio.move-sound');
 const startSound = document.querySelector('audio.start-sound');
 const stopSound = document.querySelector('audio.stop-sound');
+const totalTimeEl = document.querySelector('#totalTime');
 
 const startBtn = document.querySelector("#button button");
 const column = document.querySelector(".column");
 const columnInput = column.querySelector('#columnNumber');
+const intervalInput = column.querySelector('#intervalTime');
 
-const intervalTime = [15,10,10,15,15,10,25,10,20,25];
+const kreplinOpt = document.querySelector('#krapelin')
+const pauliOpt = document.querySelector('#pauli')
+
+
+const intervalTime = [15,10,10,15,15,10,25,20,10,20,25];
+let totalTime = 0;
 let randomTime;
-let time;
-let countdownInterval;
+let countdownInterval,time,minute = 0,second,duration = 0;
+
 let totalColumns;
+let type = "krapelin";
 
 
+pauliOpt.addEventListener('click',hideInputFild);
+kreplinOpt.addEventListener('click',hideInputFild);
 startBtn.addEventListener('click',countdown);
 
+
+
+function hideInputFild(e){
+  if (e.target.id == "pauli") {
+    type = "pauli";
+    columnInput.classList.add('hide-input');
+  }else if (e.target.id == "krapelin") {
+    type = "krapelin";
+    columnInput.classList.remove('hide-input');
+  }
+}
 
 
 // generate random interval time
@@ -26,42 +47,65 @@ function generateInterval(){
 }
 
 function countdown() {
-  if (!columnInput.value) {
+  if (!columnInput.value && type == "krapelin") {
     alert('ups! anda belum menentukan jumlah kolom')
   } else {
-    
     // hide input column
     column.classList.add('hide');
-    // hide this button
+    // hide start button
     startBtn.classList.add('hide');
     // start sound on
     startSound.play();
-    
-    totalColumns = +columnInput.value;
 
-    time = generateInterval();
+
+    totalColumns = +columnInput.value || 99999999;
+    time = +intervalInput.value || generateInterval();
+    countdownEl.classList.remove("hide");
     countdownEl.innerText = time;
     countdownInterval = setInterval(function() {
       time--;
+      duration++;
+      
+      minute = Math.floor(duration / 60);
+      second = duration % 60;
+      totalTimeEl.innerHTML = `
+       <span> total waktu: <strong> ${minute} menit ${second} detik </strong></span>
+      `;
+      
       if (time < 1) {
         totalColumns--;
         moveSound.play();
-        time = generateInterval();
+        time = +intervalInput.value || generateInterval();
         if (totalColumns < 1) {
-          // hide input column
+          // display input column
           column.classList.remove('hide');
-          // hide this button
+          // display start button
           startBtn.classList.remove('hide');
-          
-          stopSound.play();
-          moveSound.pause();
-          moveSound.currentTime = 0;
-          clearInterval(countdownInterval);
+          countdownEl.classList.add("hide");
 
+          moveSound.pause();
+          stopSound.play();
+          clearInterval(countdownInterval);
         }
       }
+      
+      if (minute == 60) {
+        // display input column
+        column.classList.remove('hide');
+        // display start button
+        startBtn.classList.remove('hide');
+        // hide countdown
+        countdownEl.classList.add('hide');
+        moveSound.pause();
+        stopSound.play();
+        duration = 0;
+        clearInterval(countdownInterval);
+      }
+      
       countdownEl.innerText = time;
+    
     }, 1000);
-
+    
   }
+ 
 }
